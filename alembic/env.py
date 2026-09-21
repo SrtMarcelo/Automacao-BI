d9cb2fd218e03d34fc3e754a50094b1dc0baf69a
Marcelo <mekanics153@gmail.com>
2026-09-21 10:09:03 -0300

    Fix linting and formatting errors

--- alembic/env.py
+++ alembic/env.py
@@ -2,8 +2,9 @@ from __future__ import annotations
 
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import engine_from_config, pool
+
+from alembic import context
 
 # add your model's MetaData object here
 # for 'autogenerate' support
